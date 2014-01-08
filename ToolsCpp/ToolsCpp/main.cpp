05e8d45414936fffab2d09922a6f40cc54b7ee6e
Guang Yang <yg398701842@live.cn>
2014-01-08 23:55:52 +0800

    First commit

---
+++ ToolsCpp/ToolsCpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ToolsCpp
+//
+//  Created by Liu Jianming on 14-1-8.
+//  Copyright (c) 2014年 Alex Y. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
